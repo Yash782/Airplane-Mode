6df7398cc1f14d0a0f7148224c65b72c871f4940
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-23 17:56:26 +0530

    Added Rent Invoice Doctype

--- airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.py
+++ airplane_mode/airport_shop_management/doctype/rent_payment/rent_payment.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2024, Yash Wadgaonkar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class RentPayment(Document):
-	pass
+    def before_submit(self):
+        if self.invoice_number:
+            try:
+                frappe.db.set_value('Rent Invoice', self.invoice_number, 'status', 'Paid')
+            except Exception as e:
+                frappe.throw(_("Failed to update Rent Invoice status: {0}").format(str(e)))
+        else:
+            frappe.logger().info("No linked Rent Invoice found for Rent Payment.")
