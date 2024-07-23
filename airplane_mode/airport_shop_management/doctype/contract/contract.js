0e7b8f832f0cf3807ff9e8fd2c5548260365125d
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-23 18:34:26 +0530

    Added "Make Rent Invoice" Button in Contract Doctype

--- airplane_mode/airport_shop_management/doctype/contract/contract.js
+++ airplane_mode/airport_shop_management/doctype/contract/contract.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Yash Wadgaonkar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
+ frappe.ui.form.on("Contract", {
+ 	refresh(frm) {
+        frm.add_custom_button(__("Make Rent Invoice"), function() {  
+            let contractId = frm.doc.name;
+            let url = `http://yash.localhost:8000/app/rent-invoice/new-rent-invoice?contract_id=${contractId}`;
+            window.open(url, '_blank');
+        });
+ 	},
+});
