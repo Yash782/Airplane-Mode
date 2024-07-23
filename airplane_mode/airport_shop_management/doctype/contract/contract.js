d661862db3168231a0c109f382c56d6583e03971
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-23 18:38:08 +0530

    Indentation Change

--- airplane_mode/airport_shop_management/doctype/contract/contract.js
+++ airplane_mode/airport_shop_management/doctype/contract/contract.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Yash Wadgaonkar and contributors
 // For license information, please see license.txt
 
- frappe.ui.form.on("Contract", {
+frappe.ui.form.on("Contract", {
  	refresh(frm) {
         frm.add_custom_button(__("Make Rent Invoice"), function() {  
             let contractId = frm.doc.name;
