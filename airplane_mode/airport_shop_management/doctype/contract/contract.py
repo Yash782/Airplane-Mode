876507478b893463ca855cf3b51eab8de8d6885a
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-24 18:13:39 +0530

    Added filters and buttons in some doctypes

--- airplane_mode/airport_shop_management/doctype/contract/contract.py
+++ airplane_mode/airport_shop_management/doctype/contract/contract.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2024, Yash Wadgaonkar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Contract(Document):
-	pass
+
+	def before_submit(self):
+		
+		shop = frappe.get_doc("Shop", self.shop)
+		shopStatus = shop.status
+		newTenant = self.tenant
+
+		if shopStatus == "Available":
+			shop.status = "Occupied"
+			shop.tenant = newTenant
+			shop.save()
+
+	def before_save(self):
+		shop = frappe.get_doc("Shop", self.shop)
+		shopStatus = shop.status
+		if shopStatus == "Occupied":
+			frappe.throw("Shop is already occupied")
+
+	def on_cancel(self):
+
+		shop = frappe.get_doc("Shop", self.shop)
+		shopStatus = shop.status
+
+		if shopStatus == "Occupied":
+			shop.status = "Available" 
+			shop.tenant = None
+			shop.save()
+			
--- airplane_mode/airport_shop_management/doctype/rent_invoice/rent_invoice.js
+++ airplane_mode/airport_shop_management/doctype/rent_invoice/rent_invoice.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Yash Wadgaonkar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rent Invoice", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Rent Invoice", {
+	refresh: function(frm) {
+        frm.add_custom_button(__("Make Payment Entry"), function() {
+            
+            let invNumber = frm.doc.contract_id;
+            const siteUrl = window.location.origin;
+            let url = `${siteUrl}/app/rent-payment/new-rent-payment?invoice_number=${invNumber}`;
+            window.open(url, '_blank');
+        });
+    },
+});
